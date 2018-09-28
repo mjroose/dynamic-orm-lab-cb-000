67a1a0e9c2233071d4d1fcca67daf5b0639247a4
Michael Roose <mjroose@gmail.com>
2018-09-28 21:06:15 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -48,7 +48,7 @@ class InteractiveRecord
 
   def self.find_by_name(name)
     sql = <<-SQL
-      SELECT * FROM #{table_name_for_insert}
+      SELECT * FROM #{table_name}
       WHERE name = #{name}
     SQL
 
