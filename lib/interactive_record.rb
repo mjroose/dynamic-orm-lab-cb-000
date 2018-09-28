c579abeeb282c393c2d71a2fc53b3270b3ddb6cf
Michael Roose <mjroose@gmail.com>
2018-09-28 21:17:56 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -60,7 +60,7 @@ class InteractiveRecord
     value = options.values[0].is_a? Numeric ? options.values[0] : '#{options.values[0]}'
     sql = <<-SQL
       SELECT * FROM #{table_name}
-      WHERE #{options.keys[0]} = '#{options.values[0]}';
+      WHERE #{column} = '#{value}';
     SQL
     binding.pry
     DB[:conn].execute(sql)
