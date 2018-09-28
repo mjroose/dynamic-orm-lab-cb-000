cb04cd8b8a336d6d94ff0cd9b4f7fafb422d992d
Michael Roose <mjroose@gmail.com>
2018-09-28 20:22:25 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -5,4 +5,8 @@ class InteractiveRecord
   def self.table_name
     self.to_s.downcase.pluralize
   end
+
+  def self.column_names
+    table_info = DB[:conn].execute("pragma table_info(#{table_name});")
+  end
 end
